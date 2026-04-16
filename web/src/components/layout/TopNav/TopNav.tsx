e8c245145f0158fa8299c5c3c7cc8d8e9e91ead4
SAPAAAA <78519771+SAPAAAA@users.noreply.github.com>
2026-04-17 05:06:31 +0700

    feat: added auth view

--- web/src/components/layout/TopNav/TopNav.tsx
+++ web/src/components/layout/TopNav/TopNav.tsx
@@ -1,8 +1,9 @@
-import { memo } from "react";
-import { NavLink } from "react-router";
+import { memo, useState } from "react";
+import { NavLink, Link } from "react-router";
 
 import "./TopNav.css";
 import useCartSidebar from "@/hooks/useCartSidebar";
+import useAuth from "@/hooks/useAuth";
 
 interface TopNavProps {}
 
@@ -15,6 +16,13 @@ const TABS = [
 
 const TopNav = memo(({}: Readonly<TopNavProps>) => {
   const { openSidebar } = useCartSidebar();
+  const { user, logout } = useAuth();
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const handleLogout = () => {
+    logout();
+    setIsDropdownOpen(false);
+  };
 
   return (
     <nav className="nav">
@@ -34,6 +42,7 @@ const TopNav = memo(({}: Readonly<TopNavProps>) => {
           );
         })}
       </ul>
+      
       <div className="nav-cart">
         <button
           className="cart-button"
@@ -51,6 +60,35 @@ const TopNav = memo(({}: Readonly<TopNavProps>) => {
             <path d="M5.929 1.757a.5.5 0 1 0-.858-.514L2.217 6H.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h.623l1.844 6.456A.75.75 0 0 0 3.69 15h8.622a.75.75 0 0 0 .722-.544L14.877 8h.623a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1.717L10.93 1.243a.5.5 0 1 0-.858.514L12.617 6H3.383zM4 10a1 1 0 0 1 2 0v2a1 1 0 1 1-2 0zm3 0a1 1 0 0 1 2 0v2a1 1 0 1 1-2 0zm4-1a1 1 0 0 1 1 1v2a1 1 0 1 1-2 0v-2a1 1 0 0 1 1-1" />
           </svg>
         </button>
+      </div>
+
+      <div className="nav-user">
+        {user ? (
+          <div className="user-menu-container">
+            <button 
+              className="user-icon-btn" 
+              onClick={() => setIsDropdownOpen((prev) => !prev)}
+              aria-label="User menu"
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
+                <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+              </svg>
+            </button>
+            
+            {isDropdownOpen && (
+              <div className="user-dropdown">
+                <div className="user-dropdown-header">
+                  <span className="user-dropdown-name">Hello, {user.firstName || 'User'}!</span>
+                </div>
+                <button className="user-dropdown-item" onClick={handleLogout}>
+                  Logout
+                </button>
+              </div>
+            )}
+          </div>
+        ) : (
+          <Link to="/login" className="login-link">Login</Link>
+        )}
       </div>
     </nav>
   );
