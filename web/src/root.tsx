1fbd40b8417345efd63f16c5cd90d5555c65fa2e
SAPAAAA <78519771+SAPAAAA@users.noreply.github.com>
2026-04-17 05:00:53 +0700

    fix: forgot to apply to track user

--- web/src/root.tsx
+++ web/src/root.tsx
@@ -8,6 +8,7 @@ import {
 } from "react-router";
 import type { Route } from "./+types/root";
 import "./global.css";
+import AuthProvider from "./providers/AuthProvider";
 
 export function HydrateFallback() {
   return (
@@ -30,7 +31,7 @@ export function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
         <Links />
       </head>
       <body>
-        {children}
+        <AuthProvider>{children}</AuthProvider>
         <ScrollRestoration />
         <Scripts />
       </body>
