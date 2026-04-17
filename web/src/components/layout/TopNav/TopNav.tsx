32e581660a47d05f023a81ce1115fb89d2762bc7
SAPAAAA <78519771+SAPAAAA@users.noreply.github.com>
2026-04-17 19:46:35 +0700

    feat: conditionally render Sidebar in MainLayout based on user authentication

--- web/src/components/layout/TopNav/TopNav.tsx
+++ web/src/components/layout/TopNav/TopNav.tsx
@@ -43,42 +43,54 @@ const TopNav = memo(({}: Readonly<TopNavProps>) => {
         })}
       </ul>
       
-      <div className="nav-cart">
-        <button
-          className="cart-button"
-          type="button"
-          aria-label="Open cart"
-          onClick={() => openSidebar()}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            fill="currentColor"
-            viewBox="0 0 16 16"
+      {/* Cart icon for logged in users */}
+      {user && (
+        <div className="nav-cart">
+          <button
+            className="cart-button"
+            type="button"
+            aria-label="Open cart"
+            onClick={() => openSidebar()}
           >
-            <path d="M5.929 1.757a.5.5 0 1 0-.858-.514L2.217 6H.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h.623l1.844 6.456A.75.75 0 0 0 3.69 15h8.622a.75.75 0 0 0 .722-.544L14.877 8h.623a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1.717L10.93 1.243a.5.5 0 1 0-.858.514L12.617 6H3.383zM4 10a1 1 0 0 1 2 0v2a1 1 0 1 1-2 0zm3 0a1 1 0 0 1 2 0v2a1 1 0 1 1-2 0zm4-1a1 1 0 0 1 1 1v2a1 1 0 1 1-2 0v-2a1 1 0 0 1 1-1" />
-          </svg>
-        </button>
-      </div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              fill="currentColor"
+              viewBox="0 0 16 16"
+            >
+              <path d="M5.929 1.757a.5.5 0 1 0-.858-.514L2.217 6H.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h.623l1.844 6.456A.75.75 0 0 0 3.69 15h8.622a.75.75 0 0 0 .722-.544L14.877 8h.623a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1.717L10.93 1.243a.5.5 0 1 0-.858.514L12.617 6H3.383zM4 10a1 1 0 0 1 2 0v2a1 1 0 1 1-2 0zm3 0a1 1 0 0 1 2 0v2a1 1 0 1 1-2 0zm4-1a1 1 0 0 1 1 1v2a1 1 0 1 1-2 0v-2a1 1 0 0 1 1-1" />
+            </svg>
+          </button>
+        </div>
+      )}
 
+      {/* User menu */}
       <div className="nav-user">
         {user ? (
           <div className="user-menu-container">
-            <button 
-              className="user-icon-btn" 
+            <button
+              className="user-icon-btn"
               onClick={() => setIsDropdownOpen((prev) => !prev)}
               aria-label="User menu"
             >
-              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
-                <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                fill="currentColor"
+                viewBox="0 0 16 16"
+              >
+                <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
               </svg>
             </button>
-            
+
             {isDropdownOpen && (
               <div className="user-dropdown">
                 <div className="user-dropdown-header">
-                  <span className="user-dropdown-name">Hello, {user.firstName || 'User'}!</span>
+                  <span className="user-dropdown-name">
+                    Hello, {user.firstName || "User"}!
+                  </span>
                 </div>
                 <button className="user-dropdown-item" onClick={handleLogout}>
                   Logout
@@ -87,7 +99,9 @@ const TopNav = memo(({}: Readonly<TopNavProps>) => {
             )}
           </div>
         ) : (
-          <Link to="/login" className="login-link">Login</Link>
+          <Link to="/login" className="login-link">
+            Login
+          </Link>
         )}
       </div>
     </nav>
--- web/src/layouts/MainLayout/MainLayout.tsx
+++ web/src/layouts/MainLayout/MainLayout.tsx
@@ -3,14 +3,18 @@ import { Outlet } from "react-router";
 import "./MainLayout.css";
 import Sidebar from "@/components/layout/CartSidebar";
 import CartSidebarProvider from "@/providers/CartSidebarProvider";
+import useAuth from "@/hooks/useAuth";
 
 const MainLayout = () => {
+  const { user } = useAuth();
+
   return (
     <CartSidebarProvider>
       <div className="main-layout">
         <Header />
         <Outlet />
-        <Sidebar />
+        {/* Only render the actual sidebar drawer if the user is logged in */}
+        {user && <Sidebar />}
       </div>
     </CartSidebarProvider>
   );
