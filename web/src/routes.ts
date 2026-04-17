1acdd021bedc2157e8997ea35de30088bdb561c6
SAPAAAA <78519771+SAPAAAA@users.noreply.github.com>
2026-04-18 03:07:06 +0700

    nothing

--- web/src/routes.ts
+++ web/src/routes.ts
@@ -10,17 +10,19 @@ export default [
   layout("./layouts/MainLayout/index.ts", [
     index("./routes/Home/index.ts"),
     route("menu/*", "./features/drinks/pages/Menu/index.ts"),
-    layout(
-      "./layouts/CustomerLayout/index.ts",
-      [route("checkout", "./features/orders/pages/Checkout/index.ts")],
-    ),
+    layout("./layouts/CustomerLayout/index.ts", [
+      route("checkout", "./features/orders/pages/Checkout/index.ts"),
+      route(
+        "orders/confirmation/:orderId",
+        "./features/orders/pages/Confirmation/index.ts",
+      ),
+    ]),
   ]),
 
   // Barista routes
-  layout(
-    "./layouts/BaristaLayout/index.ts",
-    [route("barista", "./features/orders/pages/BaristaBoard/index.ts")],
-  ),
+  layout("./layouts/BaristaLayout/index.ts", [
+    route("barista", "./features/orders/pages/BaristaBoard/index.ts"),
+  ]),
 
   // Authentication routes
   layout("./layouts/AuthLayout/index.ts", [
